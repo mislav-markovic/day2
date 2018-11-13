bc7989b579426574bc02c958d1ef1ab29d402d7d
Mislav <mislav.markovic@avl.com>
2018-11-13 17:48:39 +0100

    Solution for day 2 exercise 2

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,53 @@
 #include "array.h"
+#include <stdexcept>
+#include <algorithm>
+
+array::array() : p_{nullptr}, n_{0}
+{
+}
+
+array::array(int size, double value) : p_{new double[size]}, n_{size}
+{
+  std::fill_n(p_, size, value);
+}
+
+array::array(const array& other) : array(other.n_, other.at(0))
+{
+}
+
+array::array(array&& other) noexcept : p_{other.p_}, n_{other.n_}
+{
+  other.p_ = nullptr;
+  other.n_ = 0;
+}
+
+array::~array()
+{
+  delete[] p_;
+  p_ = nullptr;
+}
+
+int array::size() const
+{
+  return n_;
+}
+
+double array::at(int index) const
+{
+  if (index < 0 || index >= n_) throw std::out_of_range("Index is out of range");
+  return p_[index];
+}
+
+array& array::operator=(const array& other)
+{
+  if(this != &other)
+  {
+    delete[] p_;    
+    n_ = other.n_;
+    p_ = new double[n_];
+
+    std::fill_n(p_, n_, other.at(0));
+  };
+
+  return *this;
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -1,7 +1,20 @@
+#pragma once
+
 // do not use standard container as member or base class
 class array
 {
-	double* p;
-	int n;
+private:
+  double* p_;
+  int n_;
 public:
+  array();
+  array(int size, double value);
+  array(const array& other);
+  array(array&& other) noexcept;
+  ~array();
+
+  int size() const;
+  double at(int index) const;
+
+  array& operator=(const array& other);
 };
