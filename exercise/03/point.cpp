2e42b325a9e576e0946daf9d7b26c901d29886c5
Mislav <mislav.markovic@fer.hr>
2018-11-13 21:31:11 +0100

    Solution for exercise 3

--- exercise/03/point.cpp
+++ exercise/03/point.cpp
@@ -1,2 +1,40 @@
 #include "point.h"
 
+point::point() : point(0, 0)
+{
+}
+
+point::point(int x, int y) : x_{x}, y_{y}
+{
+}
+
+bool point::operator==(const point& other) const
+{
+  return x_ == other.x_ && y_ == other.y_;
+}
+
+bool point::operator!=(const point& other) const
+{
+  return !(*this == other);
+}
+
+bool point::operator<(const point& other) const
+{
+  if (x_ == other.x_) return y_ < other.y_;
+  return x_ < other.x_;
+}
+
+bool point::operator>=(const point& other) const
+{
+	return !(*this < other);
+}
+
+bool point::operator>(const point& other) const
+{
+	return !(*this < other || *this == other);
+}
+
+bool point::operator<=(const point& other) const
+{
+	return !(*this > other);
+}
---
+++ exercise/03/point.h
@@ -0,0 +1,18 @@
+class point
+{
+private:
+  int x_;
+  int y_;
+
+public:
+  point();
+  point(int x, int y);
+
+  bool operator==(const point& other) const;
+  bool operator!=(const point& other) const;
+  bool operator<(const point& other) const;
+  bool operator>=(const point& other) const;
+  bool operator>(const point& other) const;
+  bool operator<=(const point& other) const;
+
+};
